08055311458711e0d46a14ba78b09311aec7dd7d
nfadeaway <farappl@gmail.com>
2023-09-23 12:53:29 +0500

    fix tests

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -1,13 +1,13 @@
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('../webpack.prod.js');
+const webpack = require("webpack");
+const WebpackDevServer = require("webpack-dev-server");
+const config = require("../webpack.prod.js");
 
 const server = new WebpackDevServer(webpack(config), {});
-server.listen(9000, 'localhost', (err) => {
+server.listen(9000, "localhost", (err) => {
   if (err) {
     return;
   }
   if (process.send) {
-    process.send('ok');
+    process.send("ok");
   }
 });
